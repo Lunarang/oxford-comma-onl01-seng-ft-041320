0de2ed1b2facc364f7643984a6b52b2b97e45f6f
Erin Mettille <ecmettille@gmail.com>
2020-04-16 21:55:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif array.size == 2
       array.join(" and ")
     else
-      array.last = done
+      done = array.last
       array >> array.last
       array << "and #{done}"
       array.join(", ")
